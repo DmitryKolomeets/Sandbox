5d5573585f4f78ec5388dc5b1ccf58ac02f1bc17
Dmitry Kolomeets <bde@ukr.net>
2022-08-03 21:30:08 +0300

    method to input three numbers

--- src/main/java/inputThreeNumbers/Solution.java
+++ src/main/java/inputThreeNumbers/Solution.java
@@ -5,10 +5,50 @@ package inputThreeNumbers;
 //        Написать двумя способами. If и switch.
 
 
+import java.util.Scanner;
+
 public class Solution {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter one number by choosing from 1 or 2 or 3");
+        int num = in.nextInt();
 
 
+//                                    IF
+//        ____________________________________________________________________________________
+//        if (num == 1) {
+//            System.out.println("Your number is 1");
+//
+//        } else if (num == 2) {
+//            System.out.println("Your number is 2");
+//        }
+//        else if (num == 3) {
+//            System.out.println("Your number is 3");
+//        } else {
+//            System.out.println("Wrong number. Your number is not 1 or 2 or 3. Please, enter correct number.");
+//        }
+//    }
+
+
+        //                                    SWITCH
+//        ____________________________________________________________________________________
+
+        switch (num) {
+
+            case 1:
+                System.out.println("Your number is 1");
+                break;
+            case 2:
+                System.out.println("Your number is 2");
+                break;
+            case 3:
+                System.out.println("Your number is 3");
+                break;
+            default:
+                System.out.println("Wrong number. Your number is not 1 or 2 or 3. Please, enter correct number.");
+        }
+    }
 }
 
 
