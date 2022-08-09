050fe7117e8b256d70b95f8031c5403568edcdb1
Dmitry Kolomeets <bde@ukr.net>
2022-08-09 21:43:07 +0300

    added Enum and Price method

--- src/main/java/drinksMashin/DrinksMashine.java
+++ src/main/java/drinksMashin/DrinksMashine.java
@@ -1,4 +1,26 @@
 package drinksMashin;
 
 public enum DrinksMashine {
+
+
+    COFFEE("Coffee"),
+    TEA("Tea"),
+    LEMONADE("Lemonade"),
+    MOJITO("Mojito"),
+    MINERAL_WATER("Mineral water"),
+    COCA_COLA ("Coca Cola");
+
+    public String drinkType;
+
+    DrinksMashine(String drinkType) {
+        this.drinkType = drinkType;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DrinksMashine{" +
+                "drinkType='" + drinkType + '\'' +
+                '}';
+    }
 }
