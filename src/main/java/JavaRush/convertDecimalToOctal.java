053276875e529f5cb24ddd0e3fd54a67974c24ad
Dmitry Kolomeets <bde@ukr.net>
2023-04-22 15:53:27 +0200

    TriangleArray

---
+++ src/main/java/JavaRush/convertDecimalToOctal.java
@@ -0,0 +1,58 @@
+package JavaRush;
+
+
+
+
+public class convertDecimalToOctal {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Десятичное число " + decimalNumber + " равно восьмеричному числу " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Восьмеричное число " + octalNumber + " равно десятичному числу " + toDecimal(octalNumber));
+    }
+
+
+    public static int toOctal(int decimalNumber) {
+        if (decimalNumber > 0) {
+            int i = 0;
+            int octalNumber = 0;
+            while (decimalNumber != 0) {
+                octalNumber = (int) (octalNumber + (decimalNumber % 8) * Math.pow(10, i));
+                decimalNumber = decimalNumber / 8;
+                i++;
+            }
+            return octalNumber;
+        } else {
+            return 0;
+        }
+
+    }
+
+
+    public static int toDecimal(int octalNumber) {
+        if (octalNumber > 0) {
+            int i = 0;
+            int decimalNumber = 0;
+            while (octalNumber != 0) {
+                decimalNumber = (int) (decimalNumber + (octalNumber % 10) * Math.pow(8, i));
+                octalNumber = octalNumber / 10;
+                i++;
+
+            }
+
+            return decimalNumber;
+        } else {
+            return 0;
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
