6072cd78eaf09bd7c3979e8dbd004013587f59da
Dmitry Kolomeets <bde@ukr.net>
2023-05-09 09:58:01 +0200

    Javarush_level_13

---
+++ src/main/java/JavaRush/JavaRush_12/Sudent/Solution.java
@@ -0,0 +1,9 @@
+package JavaRush.JavaRush_12.Sudent;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Student student = new Student("Амиго");
+        System.out.println(student.name);
+    }
+}
---
+++ src/main/java/JavaRush/JavaRush_12/Sudent/Student.java
@@ -0,0 +1,10 @@
+package JavaRush.JavaRush_12.Sudent;
+
+public class Student {
+
+     String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
