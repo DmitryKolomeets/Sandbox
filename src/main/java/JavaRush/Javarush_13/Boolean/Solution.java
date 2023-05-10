a39c937d77071f05af398cdcd82ca54d47039b81
Dmitry Kolomeets <bde@ukr.net>
2023-05-10 11:28:56 +0200

    Javarush_level_13

---
+++ src/main/java/JavaRush/Javarush_13/Boolean/Solution.java
@@ -0,0 +1,21 @@
+package JavaRush.Javarush_13.Boolean;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 1;
+
+        Boolean isLess = isLess(first, second);
+
+        if (isLess) {
+            System.out.println("Первое число меньше второго");
+        } else {
+            System.out.println("Первое число больше или равно второму числу");
+        }
+    }
+
+    public static Boolean isLess(int first, int second) {
+        return first < second ? true : false;
+    }
+}
