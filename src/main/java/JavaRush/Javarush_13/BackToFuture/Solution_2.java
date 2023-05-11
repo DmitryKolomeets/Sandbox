c3467fe3bf554c1eae600933374ce62fe489f72f
Dmitry Kolomeets <bde@ukr.net>
2023-05-11 10:06:33 +0200

    Javarush_level_13

---
+++ src/main/java/JavaRush/Javarush_13/BackToFuture/Solution_2.java
@@ -0,0 +1,22 @@
+package JavaRush.Javarush_13.BackToFuture;
+
+import java.util.ArrayList;
+
+public class Solution_2 {
+
+    private int [] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
---
+++ src/main/java/JavaRush/Javarush_13/Diamond/Solution.java
@@ -0,0 +1,19 @@
+package JavaRush.Javarush_13.Diamond;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
