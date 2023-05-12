ada8e260c6060ad4c0ed6b0c7c0fb6de361fd784
Dmitry Kolomeets <bde@ukr.net>
2023-05-12 11:43:21 +0200

    Javarush_level_14

--- src/main/java/JavaRush/JavaRush_10/CheckProtocol/Solution.java
+++ src/main/java/JavaRush/JavaRush_10/CheckProtocol/Solution.java
@@ -2,7 +2,25 @@ package JavaRush.JavaRush_10.CheckProtocol;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
 
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
+
+    public static String checkDomain(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
 
 
 }
