dd1f4acca5a3a347a9549ad5ddb2dea41275e1fb
Dmitry Kolomeets <bde@ukr.net>
2023-06-09 22:28:13 +0200

    Javarush_level_16

---
+++ src/main/java/JavaRush/JavaRush_16/SurfacePrograming/Solution.java
@@ -0,0 +1,29 @@
+package JavaRush.JavaRush_16.SurfacePrograming;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(sourceDirectory)) {
+            for (Path path : files) {
+                if (Files.isRegularFile(path)) {
+                    Path resolve = targetDirectory.resolve(path.getFileName());
+                    Files.copy(path, resolve);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("poiman exception");
+        }
+
+
+    }
+}
