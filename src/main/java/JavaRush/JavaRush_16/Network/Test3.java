75b9c91fecf492b3f2beaccf54a9771bb20b2457
Dmitry Kolomeets <bde@ukr.net>
2023-06-10 21:42:39 +0200

    Javarush_level_16

---
+++ src/main/java/JavaRush/JavaRush_16/Network/Test3.java
@@ -0,0 +1,24 @@
+package JavaRush.JavaRush_16.Network;
+
+import java.io.FilterOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        OutputStream stream = System.out;
+
+        FilterOutputStream stream2 = System.out;
+
+
+        PrintStream stream3 = System.out;
+
+
+
+
+    }
+
+}
