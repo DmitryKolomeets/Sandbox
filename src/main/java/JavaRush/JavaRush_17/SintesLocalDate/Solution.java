a17f33c12e569fae1b4537675cd4c9f2b0d5b618
Dmitry Kolomeets <bde@ukr.net>
2023-06-13 10:17:56 +0200

    Javarush_level_16

---
+++ src/main/java/JavaRush/JavaRush_17/SintesLocalDate/Solution.java
@@ -0,0 +1,32 @@
+package JavaRush.JavaRush_17.SintesLocalDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<LocalDate, List<LocalTime>> dateMap = DateTimeGenerator.generateDateMap();
+        printCollection(dateMap.entrySet());
+
+        Set<LocalDateTime> dateSet = convert(dateMap);
+        printCollection(dateSet);
+    }
+
+    static Set<LocalDateTime> convert(Map<LocalDate, List<LocalTime>> sourceMap) {
+        //напишите тут ваш код
+
+        return null;
+    }
+
+    static void printCollection(Collection<?> collection) {
+        System.out.println("-----------------------------------------------------");
+        collection.forEach(System.out::println);
+    }
+
+}
