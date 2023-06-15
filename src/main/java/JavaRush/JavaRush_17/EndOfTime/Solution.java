3600fef46920c181e333c000ed33788231ce8710
Dmitry Kolomeets <bde@ukr.net>
2023-06-15 15:21:42 +0200

    Javarush_level_16

---
+++ src/main/java/JavaRush/JavaRush_17/EndOfTime/Solution.java
@@ -0,0 +1,30 @@
+package JavaRush.JavaRush_17.EndOfTime;
+
+import java.time.Instant;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getMaxFromMilliseconds());
+        System.out.println(getMaxFromSeconds());
+        System.out.println(getMaxFromSecondsAndNanos());
+    }
+
+    static Instant getMaxFromMilliseconds() {
+
+
+        return Instant.ofEpochMilli(Long.MAX_VALUE);
+    }
+
+    static Instant getMaxFromSeconds() {
+       return Instant.ofEpochSecond(Instant.MAX.getEpochSecond());
+
+    }
+
+    static Instant getMaxFromSecondsAndNanos() {
+
+
+        return Instant.ofEpochSecond(Instant.MAX.getEpochSecond(),999999999);
+    }
+
+}
