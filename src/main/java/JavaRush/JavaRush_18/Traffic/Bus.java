99337a8047161d4e12078b0e1e8f25139d8b1960
Dmitry Kolomeets <bde@ukr.net>
2023-06-23 12:50:59 +0200

    Javarush_level_17

---
+++ src/main/java/JavaRush/JavaRush_18/Traffic/Bus.java
@@ -0,0 +1,10 @@
+package JavaRush.JavaRush_18.Traffic;
+
+public class Bus implements Vehicle {
+
+    public void move() {
+        System.out.println("Еду со средней скоростью 50 км/ч.");
+    }
+
+
+}
---
+++ src/main/java/JavaRush/JavaRush_18/Traffic/Car.java
@@ -0,0 +1,12 @@
+package JavaRush.JavaRush_18.Traffic;
+
+public class Car implements Vehicle{
+
+
+
+    public void move() {
+        System.out.println("Еду со средней скоростью 70 км/ч.");
+    }
+
+
+}
