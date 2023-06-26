165bcfce5f3e946225d76ec280800d91dffff8eb
Dmitry Kolomeets <bde@ukr.net>
2023-06-26 11:01:11 +0200

    Javarush_level_17

---
+++ src/main/java/JavaRush/JavaRush_19/ConvertData_2/Solution.java
@@ -0,0 +1,21 @@
+package JavaRush.JavaRush_19.ConvertData_2;
+
+import java.util.stream.Stream;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Не", "волнуйтесь", "если", "что-то", "не", "работает.", "Если", "бы", "всё", "работало", "вас", "бы", "уволили.");
+
+        toUpperCase(stream).forEach(System.out::println);
+    }
+
+    public static Stream<String> toUpperCase(Stream<String> strings) {
+
+
+
+        return strings.map(String::toUpperCase);
+    }
+
+}
