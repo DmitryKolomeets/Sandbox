febf110356c615762ae007064c5cb37bda6a5e9f
Dmitry Kolomeets <bde@ukr.net>
2023-06-27 21:50:33 +0200

    Javarush_level_17

--- src/main/java/JavaRush/JavaRush_19/StreamToString/Solution.java
+++ src/main/java/JavaRush/JavaRush_19/StreamToString/Solution.java
@@ -1,9 +1,20 @@
 package JavaRush.JavaRush_19.StreamToString;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
 public class Solution {
 
+    public static void main(String[] args) {
+        var stringStream = Stream.of("To", "be", "a", "programmer", "you", "need", "to", "code");
 
+        System.out.println(getString(stringStream));
+    }
 
-
-
+    public static String getString(Stream<String> stringStream) {
+        return stringStream
+                .collect(joining(" "));
+    }
 }
