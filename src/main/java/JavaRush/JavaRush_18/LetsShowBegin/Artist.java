608ded66b0cb6b016a8e44b0e8772fcd087743a1
Dmitry Kolomeets <bde@ukr.net>
2023-06-28 22:24:24 +0200

    Javarush_level_17

---
+++ src/main/java/JavaRush/JavaRush_18/LetsShowBegin/Artist.java
@@ -0,0 +1,4 @@
+package JavaRush.JavaRush_18.LetsShowBegin;
+
+public interface Artist extends Dance, Sing{
+}
---
+++ src/main/java/JavaRush/JavaRush_18/SpaceOdyssey_1/Astronaut.java
@@ -0,0 +1,8 @@
+package JavaRush.JavaRush_18.SpaceOdyssey_1;
+
+public class Astronaut {
+    public String getInfo(){
+        return "Неизвестный астронавт";
+    }
+
+}
