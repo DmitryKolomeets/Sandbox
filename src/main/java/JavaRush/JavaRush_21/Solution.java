7e7d4ef002c5f6fcbbd0faa82a0a409d5e93abac
Dmitry Kolomeets <bde@ukr.net>
2023-07-06 16:40:32 +0200

    Javarush_level_20

---
+++ src/main/java/JavaRush/JavaRush_21/Solution.java
@@ -0,0 +1,16 @@
+package JavaRush.JavaRush_21;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse{
+
+    }
+
+}
