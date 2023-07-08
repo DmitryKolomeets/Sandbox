3e32d2c8f6f52991b93075f15430e245980dfbc7
Dmitry Kolomeets <bde@ukr.net>
2023-07-08 21:30:47 +0200

    Javarush_level_20

---
+++ src/main/java/JavaRush/JavaRush_20/Heritage/Main.java
@@ -0,0 +1,20 @@
+package JavaRush.JavaRush_20.Heritage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      Animal animal = new Animal();
+      Cat cat = new Cat();
+      Dog dog = new Dog();
+
+      animal.voice();
+      cat.voice();
+      dog.voice();
+
+
+
+    }
+
+
+}
