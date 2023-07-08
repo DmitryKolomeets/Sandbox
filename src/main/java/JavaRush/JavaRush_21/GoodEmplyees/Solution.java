cda05a67b9c9cafafc6e55326107c73f34891dfe
Dmitry Kolomeets <bde@ukr.net>
2023-07-08 21:29:19 +0200

    Javarush_level_20

---
+++ src/main/java/JavaRush/JavaRush_21/GoodEmplyees/Solution.java
@@ -0,0 +1,25 @@
+package JavaRush.JavaRush_21.GoodEmplyees;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee{
+
+    }
+
+    public class Chief extends Employee{
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee{
+
+    }
+
+
+}
