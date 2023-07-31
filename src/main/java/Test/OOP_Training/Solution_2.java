593c0d4a72dcbaeae0febc3322ed9eb6ed034e8d
Dmitry Kolomeets <bde@ukr.net>
2023-07-31 21:18:29 +0200

    OOP Training 2

---
+++ src/main/java/Test/OOP_Training/Solution_2.java
@@ -0,0 +1,38 @@
+package Test.OOP_Training;
+
+public class Solution_2 {
+
+
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+
+        callFoodMethods(food);
+        callSelectableMethods(selectable);
+    }
+
+    public static void callFoodMethods(Food food) {
+        food.onEat();
+        food.onSelect();
+    }
+
+    public static void callSelectableMethods(Selectable selectable) {
+        selectable.onSelect();
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food implements Selectable{
+        public void onEat() {
+            System.out.println("The food was eaten");
+        }
+
+        @Override
+        public void onSelect() {
+            System.out.println("The food was selected");
+        }
+    }
+
+}
