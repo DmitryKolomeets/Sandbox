c0af4f68a16007cd5b75d06f678c14d96abdd925
Dmitry Kolomeets <bde@ukr.net>
2023-08-09 10:51:04 +0200

    Something New 3

---
+++ src/main/java/Test/OOP_Training/HenFactory.java
@@ -0,0 +1,44 @@
+package Test.OOP_Training;
+
+public class HenFactory {
+
+//    public static void main(String[] args) {
+//
+//        Hen hen = HenFactory.getHen(Country.BELARUS);
+//        hen.getCountOfEggsPerMonth();
+//
+//
+//        Hen hen2 = HenFactory.getHen(Country.RUSSIA);
+//        hen2.getCountOfEggsPerMonth();
+//
+//
+//
+//
+//    }
+//
+//    static class HenFactory {
+//        static Hen getHen(String country) {
+//            Hen hen = null;
+//
+//            switch (country) {
+//                case Country.BELARUS:
+//                    hen = new BelarusianHen();
+//                    break;
+//                case Country.RUSSIA:
+//                    hen = new RussianHen();
+//                    break;
+//
+//                case Country.MOLDOVA:
+//                    hen = new MoldovanHen();
+//                    break;
+//
+//                case Country.UKRAINE:
+//                    hen = new UkrainianHen();
+//                    break;
+//
+//            }
+//
+//            return hen;
+//        }
+//    }
+}
