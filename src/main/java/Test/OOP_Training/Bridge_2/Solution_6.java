f61d5e902614d9b12a6dfd52049aebf0083138a4
Dmitry Kolomeets <bde@ukr.net>
2023-08-13 19:58:19 +0200

    Bridges 2

---
+++ src/main/java/Test/OOP_Training/Bridge_2/Solution_6.java
@@ -0,0 +1,27 @@
+package Test.OOP_Training.Bridge_2;
+
+public class Solution_6 {
+
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore extends Animal{
+
+    }
+
+    public class Cow extends Animal{
+
+    }
+
+    public class Dog extends Carnivore{
+
+    }
+
+    public class Pig extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+}
