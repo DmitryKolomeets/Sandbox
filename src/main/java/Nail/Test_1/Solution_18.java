812a6e13c6c84034cc3d346c6c6089f2869fafae
Dmitry Kolomeets <bde@ukr.net>
2023-08-23 23:10:05 +0200

    Bridges 2

---
+++ src/main/java/Nail/Test_1/Solution_18.java
@@ -0,0 +1,17 @@
+package Nail.Test_1;
+
+public class Solution_18 {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+
+    public interface Person {
+        boolean isAlive();
+    }
+
+
+    public interface Presentable extends Person{
+
+    }
+}
