1998ce5bfb21e3c5926c9576cd31ae4b6c0c2c6b
Dmitry Kolomeets <bde@ukr.net>
2023-09-02 13:03:48 +0200

    Robot

---
+++ src/main/java/Nail/Robot/Solution_16.java
@@ -0,0 +1,26 @@
+package Nail.Robot;
+
+public class Solution_16 {
+
+
+    public static void main(String[] args) throws Exception {
+        EnglishTranslator englishTranslator = new EnglishTranslator();
+        System.out.println(englishTranslator.translate());
+    }
+
+    public static abstract class Translator {
+        public abstract String getLanguage();
+
+        public String translate() {
+            return "Я переводчик с " + getLanguage();
+        }
+    }
+
+
+    public static class EnglishTranslator extends Translator{
+        @Override
+        public String getLanguage() {
+            return "английского";
+        }
+    }
+}
