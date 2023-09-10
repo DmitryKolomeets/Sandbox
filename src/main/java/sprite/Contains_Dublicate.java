1998e9fa8b07d70e80636fc146c28184129027a6
Dmitry Kolomeets <bde@ukr.net>
2023-09-10 18:24:20 +0200

    Dublicates scaning

---
+++ src/main/java/sprite/Contains_Dublicate.java
@@ -0,0 +1,26 @@
+package sprite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contains_Dublicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], i);
+            } else {
+                int PastIndex = hm.get(nums[i]);
+                if (Math.abs(PastIndex - i) <= k) {
+                    return true;
+                } else {
+                    hm.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}
