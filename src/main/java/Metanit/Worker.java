6eff04632de44841d07d7a4fe210d409a4eaa47e
Dmitry Kolomeets <bde@ukr.net>
2023-09-22 11:12:23 +0200

    Majority

---
+++ src/main/java/Metanit/Worker.java
@@ -0,0 +1,23 @@
+package Metanit;
+
+
+
+    public  class Worker {
+        public void workHard() {
+
+        }
+
+    }
+
+//    public  interface Businessman {
+//        void workHard();
+//    }
+//
+//
+//
+//
+//
+//    public  class CTO extends Worker  implements Businessman {
+//
+//    }
+//}
