5aae124399ce5671b440097de248c60aafe9f10f
Dmitry Kolomeets <bde@ukr.net>
2023-09-24 11:20:08 +0200

    Majority

---
+++ src/main/java/Metanit/Obj_2.java
@@ -0,0 +1,26 @@
+package Metanit;
+
+public class Obj_2 {
+
+    public static class Pet {
+        protected String name;
+
+        public Pet() {
+        }
+
+        public final String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static class Cat extends Pet {
+        public void setName(String name) {
+            this.name = "";
+        }
+    }
+}
