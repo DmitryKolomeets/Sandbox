e9b292abf81db3c7be654442618f4546cc129645
Dmitry Kolomeets <bde@ukr.net>
2023-09-25 18:28:10 +0200

    Majority

---
+++ src/main/java/Metanit/Fly_3.java
@@ -0,0 +1,22 @@
+package Metanit;
+
+public class Fly_3 {
+
+
+    public static void printClasses(Object obj, Mouse mouse, GrayMouse grayMouse, Jerry jerry) {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(grayMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse {
+    }
+
+    static class GrayMouse extends Mouse {
+    }
+
+    static class Jerry extends GrayMouse {
+    }
+
+}
