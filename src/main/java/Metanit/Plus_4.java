3b15678965ad20c320af598d5fab6c6d608b8d7f
Dmitry Kolomeets <bde@ukr.net>
2023-09-29 23:13:16 +0200

    Majority

---
+++ src/main/java/Metanit/Plus_4.java
@@ -0,0 +1,24 @@
+package Metanit;
+
+import java.util.Arrays;
+
+public class Plus_4 {
+
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >=0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                System.out.println(Arrays.toString(digits));
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+
+        }
+        int [] res = new int [digits.length+1];
+        res[0] = 1;
+        System.out.println(Arrays.toString(res));
+        return res;
+    }
+}
