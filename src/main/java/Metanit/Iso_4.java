08f065a5237fc999d65af3c07b9fae2d26aae433
Dmitry Kolomeets <bde@ukr.net>
2023-10-03 12:04:03 +0200

    Majority

---
+++ src/main/java/Metanit/Iso_4.java
@@ -0,0 +1,30 @@
+package Metanit;
+
+import java.util.HashMap;
+
+public class Iso_4 {
+
+    public boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character, Character> hm = new HashMap<Character, Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hm.containsKey(s.charAt(i))) {
+
+                {
+                    if (!hm.get(s.charAt(i)).equals(t.charAt(i))) {
+                        return false;
+                    }
+                }
+
+            } else {
+                if (hm.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                hm.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+
+    }
+}
