340a86587c7478ac28b0c3ef12b9abe0e0d29660
Dmitry Kolomeets <bde@ukr.net>
2023-10-06 23:18:57 +0200

    Majority

---
+++ src/main/java/lara_dz_18/User_4.java
@@ -0,0 +1,31 @@
+package lara_dz_18;
+
+public class User_4 {
+
+    public static void main(String[] args) {
+    }
+
+    public class House extends Asset{
+
+    }
+
+    public class Cat extends Animal{
+
+    }
+
+    public class Car extends Asset{
+
+    }
+
+    public class Dog extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Asset {
+
+    }
+}
