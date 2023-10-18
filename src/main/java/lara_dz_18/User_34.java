be03c2e08049b2e5f24a52d45cc17ac2249130e6
Dmitry Kolomeets <bde@ukr.net>
2023-10-18 16:29:14 +0200

    Majority

---
+++ src/main/java/lara_dz_18/User_34.java
@@ -0,0 +1,21 @@
+package lara_dz_18;
+
+import java.awt.*;
+
+public class User_34 {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal{
+
+        public String getName() {
+            return "Fox";
+        }
+    }
+
+}
