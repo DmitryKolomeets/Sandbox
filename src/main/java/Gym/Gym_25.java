10ea327b46b38dbf4db9be03fb9cce0e4aa0ec92
Dmitry Kolomeets <bde@ukr.net>
2023-11-01 18:17:40 +0100

    Majority

---
+++ src/main/java/Gym/Gym_25.java
@@ -0,0 +1,29 @@
+package Gym;
+
+public class Gym_25 {
+
+    public static int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        int res = 0;
+
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+
+            if (middle <= x / middle) { //  middle * middle = x
+                low = middle + 1;
+                res = middle;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return res;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(8));
+    }
+}
