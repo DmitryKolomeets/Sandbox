8f4f2ce76160130444c88636d49d9f593930671d
Dmitry Kolomeets <bde@ukr.net>
2023-11-07 21:35:21 +0100

    Majority

---
+++ src/main/java/Gym/Gym_43.java
@@ -0,0 +1,28 @@
+package Gym;
+
+public class Gym_43 {
+
+    public static int strStr(String haystack, String needle) {
+
+        if (haystack.length() < needle.length())
+            return -1;
+
+        if (haystack.length() == needle.length())
+            if (!haystack.equals(needle))
+                return -1;
+            else return 0;
+
+
+        return haystack.indexOf(needle);
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(strStr("sdfsdfsadbutsad", "sdaed"));
+
+    }
+
+}
