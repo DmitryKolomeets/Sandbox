742a893453c1d409d9b320dbaf5008b691e47717
Dmitry Kolomeets <bde@ukr.net>
2023-11-11 19:30:19 +0100

    Majority

---
+++ src/main/java/Gym/Gym_47.java
@@ -0,0 +1,25 @@
+package Gym;
+
+public class Gym_47 {
+
+    public static int titleToNumber(String columnTitle) {
+        int result = 0;
+        char[] letters = columnTitle.toCharArray();
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char symbol = letters[i];
+            int number = symbol - 64;
+            result = result * 26 + number;
+
+        }
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+        int number = titleToNumber("FXSHRXW");
+        System.out.println(number);
+
+    }
+}
