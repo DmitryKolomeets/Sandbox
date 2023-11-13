2d594b1587372e4264c09313cfdc00397020803f
Dmitry Kolomeets <bde@ukr.net>
2023-11-13 19:03:57 +0100

    Majority

---
+++ src/main/java/Gym/Gym_54.java
@@ -0,0 +1,21 @@
+package Gym;
+
+public class Gym_54 {
+
+    public static int lengthOfLastWord(String s) {
+        int wordLenght =0;
+        int result = 0;
+        String[] words = s.split(" ");
+        wordLenght = words[words.length-1].length();
+        System.out.println(wordLenght);
+        return wordLenght;
+    }
+
+    public static void main(String[] args) {
+
+        lengthOfLastWord("luffy все еще джойбой");
+
+    }
+
+
+}
