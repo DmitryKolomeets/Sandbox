d13cc5dccb585f0dda2d5100aef0d96395ad0530
Dmitry Kolomeets <bde@ukr.net>
2023-11-14 21:23:07 +0100

    Majority

---
+++ src/main/java/Gym/Gym_59.java
@@ -0,0 +1,21 @@
+package Gym;
+
+public class Gym_59 {
+    public static void main(String[] args) {
+
+        String numbers = "0123456789";
+
+        System.out.println(numbers.charAt(9));
+
+        System.out.println("numbers lenght = " + numbers.length());
+
+        for (int i = numbers.length()-1; i >= 0 ; i--) {
+            System.out.println(numbers.charAt(i));
+        }
+
+
+
+
+
+    }
+}
