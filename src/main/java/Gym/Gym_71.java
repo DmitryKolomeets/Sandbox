df6bd616eb7117827d7e3ff3425993185927756e
Dmitry Kolomeets <bde@ukr.net>
2023-11-18 21:41:03 +0100

    Majority

---
+++ src/main/java/Gym/Gym_71.java
@@ -0,0 +1,28 @@
+package Gym;
+
+public class Gym_71 {
+
+    public static int missingNumber(int[] nums) {
+        int xorArray = 0;
+        for (int i : nums) {
+            xorArray ^= i;
+        }
+        int xorAll = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            xorAll ^= i;
+        }
+        int number = xorArray ^ xorAll;
+        System.out.println("number is====" + number);
+        return xorArray ^ xorAll;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        missingNumber(new int[] {0,1}) ;
+
+
+    }
+}
