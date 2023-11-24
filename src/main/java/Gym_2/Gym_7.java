0748c772e6c51c02d942e4e925503876cdf793a8
Dmitry Kolomeets <bde@ukr.net>
2023-11-24 22:07:14 +0100

    k

---
+++ src/main/java/Gym_2/Gym_7.java
@@ -0,0 +1,21 @@
+package Gym_2;
+
+public class Gym_7 {
+    public static boolean checkPerfectNumber(int num) {
+        if (num <= 1) return false;
+        int sum = 1;
+        for (int i = 2; i*i <= num; ++i) {
+            if (num % i == 0) {
+                sum += i;
+                if (i*i != num) sum += num/i;
+            }
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+
+        checkPerfectNumber(28);
+
+    }
+}
