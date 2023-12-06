94222584a3effcf586f3264cb089bfe6147b931b
Dmitry Kolomeets <bde@ukr.net>
2023-12-06 23:09:35 +0100

    k

---
+++ src/main/java/Gym_2/Gym_43.java
@@ -0,0 +1,23 @@
+package Gym_2;
+
+public class Gym_43 {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car  {
+
+    }
+
+    public class Dog  {
+
+    }
+
+}
