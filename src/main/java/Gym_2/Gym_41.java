d60691573c78e2a97112518c7273cdf42a9a18da
Dmitry Kolomeets <bde@ukr.net>
2023-12-06 23:06:14 +0100

    k

---
+++ src/main/java/Gym_2/Gym_41.java
@@ -0,0 +1,31 @@
+package Gym_2;
+
+public class Gym_41 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 12, 66, 15};
+        int min = arr[0];
+        int max = arr[0];
+        int[] x = new int[2];
+        int sumMinMax = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        x[0] = min;
+        x[1] = max;
+        sumMinMax = min + max;
+
+
+        System.out.println("min element of array = " + x[0]);
+        System.out.println("max element of array = " + x[1]);
+        System.out.println("Sum Min + Max of array = " + sumMinMax);
+    }
+
+}
