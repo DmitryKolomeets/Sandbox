cc2c148ad5de43e32247cd4ef687f93ee052d2c4
Dmitry Kolomeets <bde@ukr.net>
2023-12-13 23:10:00 +0100

    k

---
+++ src/main/java/Gym_2/Gym_56.java
@@ -0,0 +1,27 @@
+package Gym_2;
+
+public class Gym_56 {
+    public boolean isIsomorphic(String s, String t) {
+
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+
+        if(s.length()!=t.length())
+            return false;
+
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
+        }
+        return true;
+
+
+
+    }
+
+}
