c0ae44900b9acc7a814b35850cbc6bdcd3db5a4e
Dmitry Kolomeets <bde@ukr.net>
2024-01-05 17:02:49 +0100

    234

---
+++ src/main/java/Gym_2/Gym_116.java
@@ -0,0 +1,29 @@
+package Gym_2;
+
+public class Gym_116 {
+
+    public static boolean isPowerOfTwo(int n) {
+
+
+
+        if(n==0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(0));
+
+    }
+}
