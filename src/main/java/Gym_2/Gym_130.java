f8d41599917efab4fb3a224a86cec39f2c1776d6
Dmitry Kolomeets <bde@ukr.net>
2024-01-09 23:01:13 +0100

    234

---
+++ src/main/java/Gym_2/Gym_130.java
@@ -0,0 +1,18 @@
+package Gym_2;
+
+public class Gym_130 {
+
+    public static boolean checkPerfectNumber(int num) {
+        if (num <= 0) return false;
+        int sum = 0;
+        for (int i = 1; i*i <= num; ++i) {
+            if (num % i == 0) {
+                sum += i;
+                if (i*i != num) sum += num/i;
+            }
+        }
+        return sum-num == num;
+
+    }
+
+}
