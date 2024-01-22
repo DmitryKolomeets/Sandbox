822e4366ace22513c544e1f7eda2166c4f33a278
Dmitry Kolomeets <bde@ukr.net>
2024-01-22 19:51:21 +0100

    234

---
+++ src/main/java/Gym_3/Gym_34.java
@@ -0,0 +1,31 @@
+package Gym_3;
+
+import java.util.Arrays;
+
+public class Gym_34 {
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        String newS = s.replaceAll("\\p{Punct}|\\p{Space}", "");
+        char[] chars = newS.toLowerCase().toCharArray();
+
+        System.out.println(Arrays.toString(chars));
+    }
+}
