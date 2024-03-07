ab2a51fc0b18832e315c6d9b65411e2b9ce94365
Dmitry Kolomeets <bde@ukr.net>
2024-03-07 22:08:51 +0100

    f

---
+++ src/main/java/Cym_4/Gym_63.java
@@ -0,0 +1,27 @@
+package Cym_4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Gym_63 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], i);
+            } else {
+                int PastIndex = hm.get(nums[i]);
+                if (Math.abs(PastIndex - i) <= k) {
+                    return true;
+                } else {
+                    hm.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+
+}
