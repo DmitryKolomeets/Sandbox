e0f519582937cea20d18a41284e43d39b0f8bf70
Dmitry Kolomeets <bde@ukr.net>
2024-03-13 20:09:14 +0100

    f

---
+++ src/main/java/Cym_4/Gym_75.java
@@ -0,0 +1,37 @@
+package Cym_4;
+
+import java.util.Arrays;
+
+public class Gym_75 {
+
+    public static int removeDuplicates(int[] nums) {
+
+        int count = 1;
+
+        int number = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != number ) {
+                number = nums[i];
+                nums[count]=number;
+                count++;
+
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        System.out.println(count);
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+
+        removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+
+        System.out.println();
+
+
+    }
+
+}
