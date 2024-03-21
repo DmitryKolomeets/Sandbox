911f570b5bf211e900de5b3745bf78514845d8ed
Dmitry Kolomeets <bde@ukr.net>
2024-03-21 21:25:46 +0100

    f

---
+++ src/main/java/Cym_4/Gym_93.java
@@ -0,0 +1,45 @@
+package Cym_4;
+
+public class Gym_93 {
+
+    public static int removeElement(int[] nums, int val) {
+        int numbers = 0;
+        int count = 0;
+        int[] bufferArray = new int[nums.length];
+
+        for (int i = 0; i < bufferArray.length; i++) {
+            bufferArray[i] = nums [i];
+        }
+
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+
+        for (int i = 0; i < bufferArray.length; i++) {
+            if (bufferArray[i] != val) {
+                nums[count] = bufferArray[i];
+                count +=1;
+            } else {
+
+            }
+        }
+
+        System.out.println("expected Array length is ===> " + bufferArray.length);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("element " + i + " is =====> " + nums[i]);
+        }
+
+        System.out.println("number of elements is ======> " + count);
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {3, 6, 7, 8, 9, 3, 0, 3};
+        removeElement(nums, 3);
+    }
+}
