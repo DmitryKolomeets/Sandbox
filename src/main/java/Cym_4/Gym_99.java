abef8974cdafc790f07f92ef7ac366707a8796d3
Dmitry Kolomeets <bde@ukr.net>
2024-03-23 21:03:13 +0100

    f

---
+++ src/main/java/Cym_4/Gym_99.java
@@ -0,0 +1,25 @@
+package Cym_4;
+
+public class Gym_99 {
+
+    public int majorityElement(int[] nums) {
+        int cand = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (count == 0) {
+                cand = nums[i];
+            }
+
+            if (cand == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+        return cand;
+    }
+
+}
