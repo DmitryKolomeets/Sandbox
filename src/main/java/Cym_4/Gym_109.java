dd9f916e7a71a6ef9ea1f7972de96019a5b872b2
Dmitry Kolomeets <bde@ukr.net>
2024-03-27 21:45:30 +0100

    f

---
+++ src/main/java/Cym_4/Gym_109.java
@@ -0,0 +1,19 @@
+package Cym_4;
+
+public class Gym_109 {
+
+    public static int missingNumber(int[] nums) {
+        int xorAll = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xorAll^=i^nums[i];
+        }
+
+        System.out.println("number is====" + xorAll);
+        return xorAll;
+    }
+    public static void main(String[] args) {
+        missingNumber(new int[] {0,1}) ;
+
+    }
+}
