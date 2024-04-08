1257918c5e3dd5ebba3e2f76bddc91a7849511b8
Dmitry Kolomeets <bde@ukr.net>
2024-04-08 22:51:46 +0200

    f

---
+++ src/main/java/Cym_4/Gym_132.java
@@ -0,0 +1,30 @@
+package Cym_4;
+
+public class Gym_132 {
+
+
+    public static boolean isPowerOfTwo(int n) {
+
+
+
+        if(n==0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(0));
+
+    }
+}
