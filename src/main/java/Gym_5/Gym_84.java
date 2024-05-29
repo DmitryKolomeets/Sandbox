5dfcd201bcd24157d31bc1a045f2b6a5cb7fcaa8
Dmitry Kolomeets <bde@ukr.net>
2024-05-29 22:28:23 +0200

    f

---
+++ src/main/java/Gym_5/Gym_84.java
@@ -0,0 +1,20 @@
+package Gym_5;
+
+public class Gym_84 {
+
+    static public int[] twoSum(int[] numbers, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if ((numbers[i] + numbers[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+}
