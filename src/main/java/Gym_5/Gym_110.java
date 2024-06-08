1d4db8072ed4373ab6d576f314615a0f4edc73d0
Dmitry Kolomeets <bde@ukr.net>
2024-06-08 20:25:34 +0200

    f

---
+++ src/main/java/Gym_5/Gym_110.java
@@ -0,0 +1,20 @@
+package Gym_5;
+
+public class Gym_110 {
+
+    public int largestAltitude(int[] gain) {
+
+        int highest = 0;
+        int path = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            path += gain[i];
+
+            if (path > highest) {
+                highest = path;
+            }
+
+        }
+        return highest;
+    }
+}
