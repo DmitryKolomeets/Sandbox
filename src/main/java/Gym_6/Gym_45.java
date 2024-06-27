3fb5af64d99b05685b97a6ad0318da5ff55ab85f
Dmitry Kolomeets <bde@ukr.net>
2024-06-27 22:03:33 +0200

    f

---
+++ src/main/java/Gym_6/Gym_45.java
@@ -0,0 +1,26 @@
+package Gym_6;
+
+import java.util.Arrays;
+
+public class Gym_45 {
+
+    public static int largestAltitude(int[] gain) {
+
+        int[] altitudes = new int[gain.length + 1];
+
+        for (int i = 0; i < gain.length; i++) {
+            altitudes[i+1] = gain[i] + altitudes[i];
+        }
+
+        System.out.println(Arrays.toString(altitudes));
+
+        int largestAltitude = 0;
+        for (int i = 0; i < altitudes.length; i++) {
+            if (altitudes[i] > largestAltitude) {
+                largestAltitude = altitudes[i];
+            }
+        }
+        return largestAltitude;
+    }
+
+}
