d14fa82a018359b59653c1220b508ef984fdd3d5
Dmitry Kolomeets <bde@ukr.net>
2024-06-27 22:02:51 +0200

    f

---
+++ src/main/java/Gym_6/Gym_44.java
@@ -0,0 +1,21 @@
+package Gym_6;
+
+public class Gym_44 {
+
+
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+
+        for (int i = 0; i < (m - n) + 1; i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                if (haystack.substring(i, n + 1).equals(needle)) {
+                    return i;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
