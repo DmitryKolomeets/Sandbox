9cb8c5a1a2b0fddcd9d2343d62980398056534e0
Dmitry Kolomeets <bde@ukr.net>
2024-07-06 22:55:45 +0200

    f

---
+++ src/main/java/Gym_7/Gym_7.java
@@ -0,0 +1,27 @@
+package Gym_7;
+
+public class Gym_7 {
+
+
+    public static String reverseWords(String s) {
+        String result = "";
+        char ch;
+
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            result = ch + result;
+        }
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+        String word = "Privet. Kak dela?";
+
+        System.out.println(reverseWords(word));
+
+
+
+    }
+}
