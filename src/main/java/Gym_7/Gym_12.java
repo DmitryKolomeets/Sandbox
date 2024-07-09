a87afa68b222655c05e1fde03fd0442bcf9d18c4
Dmitry Kolomeets <bde@ukr.net>
2024-07-09 22:02:33 +0200

    f

---
+++ src/main/java/Gym_7/Gym_12.java
@@ -0,0 +1,25 @@
+package Gym_7;
+
+import java.util.Arrays;
+
+public class Gym_12 {
+
+    public static int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; --i) {
+            ++digits[i];
+            digits[i] %= 10;
+            if (digits[i] != 0) {
+                System.out.println(Arrays.toString(digits));
+                return digits;
+            }
+        }
+        digits = new int[n + 1];
+        digits[0] = 1;
+        System.out.println(Arrays.toString(digits));
+        return digits;
+
+
+    }
+}
