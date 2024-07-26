f4463ab0e1004603ea4bf7c8a17947c7faad83f3
Dmitry Kolomeets <bde@ukr.net>
2024-07-26 22:20:30 +0200

    f

---
+++ src/main/java/Gym_7/Gym_50.java
@@ -0,0 +1,24 @@
+package Gym_7;
+
+public class Gym_50 {
+
+    public static int climbStairs(int n) {
+        int ways = 0;
+        if(n<=0){
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else{
+            int[] steps = new int[n + 1];
+            steps[0] = 1;
+            steps[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                steps[i] = steps[i - 2] + steps[i - 1];
+            }
+            ways = steps[n];
+        }
+        return ways;
+    }
+}
