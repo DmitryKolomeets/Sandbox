5ff13c9b09f9d1ede5c25581cf3b994fe045388f
Dmitry Kolomeets <bde@ukr.net>
2024-08-27 20:47:30 +0200

    f

---
+++ src/main/java/Gym_9/Gym_24.java
@@ -0,0 +1,22 @@
+package Gym_9;
+
+public class Gym_24 {
+
+
+    // A simple recursive program to find
+    // n'th fibonacci number
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // Returns number of ways to reach s'th stair
+    static int countWays(int s)
+    {
+        return fib(s + 1);
+    }
+
+    /* Driver program to test above function */
+}
