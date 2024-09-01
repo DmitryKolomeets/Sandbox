cc632774f7a15ca51b66267a7a57ddd15ce18a12
Dmitry Kolomeets <bde@ukr.net>
2024-09-01 20:46:06 +0200

    fK

---
+++ src/main/java/Gym_9/Gym_42.java
@@ -0,0 +1,22 @@
+package Gym_9;
+
+public class Gym_42 {
+
+    // A recursive function used by countWays
+    static int countWaysUtil(int n, int m)
+    {
+        if (n <= 1)
+            return n;
+        int res = 0;
+        for (int i = 1; i <= m && i <= n; i++)
+            res += countWaysUtil(n - i, m);
+        return res;
+    }
+
+    // Returns number of ways to reach s'th stair
+    static int countWays(int s, int m)
+    {
+        return countWaysUtil(s + 1, m);
+    }
+
+}
