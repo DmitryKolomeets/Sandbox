478b1f755a75ae7dc702112f1c34639872505232
Dmitry Kolomeets <bde@ukr.net>
2024-09-16 21:39:18 +0200

    fK

---
+++ src/main/java/Gym_10/Gym_20.java
@@ -0,0 +1,21 @@
+package Gym_10;
+
+public class Gym_20 {
+
+
+    public static int missingNumber(int[] nums) {
+        int xorArray = 0;
+        for (int i : nums) {
+            xorArray ^= i;
+        }
+        int xorAll = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            xorAll ^= i;
+        }
+        int number = xorArray ^ xorAll;
+        System.out.println("number is====" + number);
+        return xorArray ^ xorAll;
+    }
+
+
+}
