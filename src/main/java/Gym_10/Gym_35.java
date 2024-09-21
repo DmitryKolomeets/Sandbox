5a8e14e887c254f3ce7971834ecf3579f6e7a5c4
Dmitry Kolomeets <bde@ukr.net>
2024-09-21 21:48:27 +0200

    fK

---
+++ src/main/java/Gym_10/Gym_35.java
@@ -0,0 +1,30 @@
+package Gym_10;
+
+public class Gym_35 {
+    public boolean isPalindrome(int x) {
+        int number = x;
+        int reversed = 0;
+        boolean result = true;
+
+        if (x < 0) {
+            result = false;
+        } else if ( x>=0 ){
+            while (number != 0) {
+                int digit = number % 10;
+                reversed = reversed * 10 + digit;
+                number /= 10;
+            }
+
+            if (x == reversed) {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+
+
+}
