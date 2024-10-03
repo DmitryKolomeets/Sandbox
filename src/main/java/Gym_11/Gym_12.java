d5e789095b6e9089b0a0846a89160c0a9b24eeb1
Dmitry Kolomeets <bde@ukr.net>
2024-10-03 22:07:00 +0200

    fK

---
+++ src/main/java/Gym_11/Gym_12.java
@@ -0,0 +1,20 @@
+package Gym_11;
+
+public class Gym_12 {
+
+    public int firstUniqChar(String s) {
+
+        int[] freq = new int[26];
+        char[] letters = s.toCharArray();
+
+        for (char c : letters) {
+            freq[c - 'a']++;
+        }
+        for (int i = 0; i < letters.length; i++) {
+            if (freq[letters[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
