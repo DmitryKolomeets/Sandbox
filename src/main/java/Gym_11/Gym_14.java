14e51f41353000b0a4ded808258d31a3f5d43155
Dmitry Kolomeets <bde@ukr.net>
2024-10-04 20:57:40 +0200

    fK

---
+++ src/main/java/Gym_11/Gym_14.java
@@ -0,0 +1,23 @@
+package Gym_11;
+
+import java.util.Arrays;
+
+public class Gym_14 {
+
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        // Loop through all the elements of the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the element is not val
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+
+
+        System.out.println(Arrays.toString(nums));
+
+        System.out.println("count is " + count);
+        return count;
+    }
+}
