31ead23cb28460e700a7c86adbf24fc6f6edd35e
Dmitry Kolomeets <bde@ukr.net>
2024-10-13 20:53:42 +0200

    fK

---
+++ src/main/java/Gym_11/Gym_42.java
@@ -0,0 +1,27 @@
+package Gym_11;
+
+import java.util.Arrays;
+
+public class Gym_42 {
+
+    public static int removeDuplicates(int[] nums) {
+
+        int count = 1;
+
+        int number = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != number ) {
+                number = nums[i];
+                nums[count]=number;
+                count++;
+
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        System.out.println(count);
+        return count;
+    }
+
+}
