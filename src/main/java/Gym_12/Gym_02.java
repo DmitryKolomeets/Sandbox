2ea3d35987f0ca7c74f09e2c9115d5f0925b2fb2
Dmitry Kolomeets <bde@ukr.net>
2024-10-14 23:04:10 +0200

    fK1

--- src/main/java/Gym_12/Gym_02.java
+++ src/main/java/Gym_12/Gym_02.java
@@ -9,14 +9,17 @@ public class Gym_02 {
         for (int i = 0; i < nums.length; i++) {
             sum = sum + nums[i];
         }
-
+        System.out.println("result");
         int actualSum = (nums.length * (nums.length + 1)) / 2;
 
         System.out.println("sum====" + sum);
         System.out.println("actualSum====" + actualSum);
-
+        System.out.println("result");
+        System.out.println("result");
+        System.out.println("result");
         int missingNumber = actualSum - sum;
         System.out.println("missingNumber====" + missingNumber);
         return missingNumber;
+
     }
 }
